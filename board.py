d1ff9ebae5dd709a3ac40db3563c3a935aa7417a
Joana Iljazi <joanailjazi@Joanas-MacBook-Pro.local>
2015-11-13 20:11:27 +0100

    improvement of board

--- board.py
+++ board.py
@@ -7,6 +7,38 @@ class Board:
         self.player2=player2
         self.bins=[4,4,4,4,4,4,4,4,4,4,4,4]
         
+    def collectPoints(self,player,lastAffected,tempBins):   #TO TEST keep track of how much in all
+        lastAffected=lastAffected+48
+
+        i=lastAffected
+        # while(self.ownBin(player,i%12)): #This would be OK if you could capture from opponent even if you end up in your own hause
+        #           print("Step ",i," backward")
+        #           i=i-1
+
+        if (self.ownBin(player,i%12)):
+            print("Step ",i," took to my own house")
+            return 0
+        
+        points=0
+        ongoingCollection=True
+
+        while(ongoingCollection):
+            print("Considering bin ", i%12)
+            if ((not self.ownBin(player,i%12)) and ((tempBins[i%12] == 2) or (tempBins[i%12] == 3))):
+                points = points + tempBins[i%12]
+                print("Earning ", tempBins[i%12])
+                tempBins[i%12] = 0
+                print("Now I have ",points, " and the board is ", tempBins)
+                i=i-1
+            else:
+                print("Stopping!")
+                ongoingCollection=False
+                if (not self.ownBin(player,i%12)):
+                    print("Ended up in own houses ", i%12)
+                elif ((tempBins[i%12] != 2) and (tempBins[i%12] != 3)):
+                    print("Stones neither 2 nor 3", tempBins[i%12])
+        return points
+
 
     def updateBoard(self,player,move):  #moves are from 0 to 5 as array index, player is 1 or 2 */
         
@@ -20,45 +52,29 @@ class Board:
         for i in range(1,stones+1):
             tempBins[(move+i)%12]=tempBins[(move+i)%12]+1
 
-        earnedPoints = collectPoints(player,(stones+move)%12,tempBins)
+        earnedPoints = self.collectPoints(player,(stones+move)%12,tempBins)
 
-        if (self.validConfiguration(tempBins)==True):
+        if (self.validConfiguration(tempBins,player)):
             self.bins=tempBins
             print(self.bins)
+            print(earnedPoints)
 
     def validConfiguration(self,tempBins,player):   #to be tested
         if (player==1):
             firstIndex = 0
-            else:
-                firstIndex = 6
-        for i in range (0,5)
+        else:
+            firstIndex = 6
+            
+        for i in range (0,5):
             if (tempBins[firstIndex+i]>0):
                 return True
         return False
 
-    def collectPoints(player,lastAffected,tempBins):   #TO TEST keep track of how much in all
-        lastAffected=lastAffected+12
-
-        i=lastAffected
-        while(ownBin(player,i%12)):
-            i=i-1
-
-        points=0
-        ongoingCollection=True
-
-        while(ongoingCollection):
-            if (!ownBin(player,i%12)) && ((tempBins[i] == 2) || (tempBins[i] == 3)):
-                points = points + tempBins[i]
-                tempBins[i] = 0
-                i=i+1
-                else:
-                    ongoingCollection=False
-
-        return points
+    
 
 
-    def ownBin(player,ownbin):
-        return(((player==1) && (0<=ownbin) && (ownbin<=5)) || ((player==2) && (6<=ownbin) && (ownbin<=11)))
+    def ownBin(self,player,ownbin):
+        return(((player==1) and (0<=ownbin) and (ownbin<=5)) or ((player==2) and (6<=ownbin) and (ownbin<=11)))
 
     
 
